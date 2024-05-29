9bae6d69fedcdf8641710ff758a5e8459b253de2
Jaime Jaramillo <jaimejaramillo.dev@gmail.com>
2024-05-29 10:25:37 -0500

    Add authentication controller with register and login endpoints

---
+++ src/BuberDinner.Api/Controllers/AuthenticationController.cs
@@ -0,0 +1,47 @@
+﻿using BuberDinner.Application.Autentication;
+using BuberDinner.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuberDinner.Api.Controllers;
+
+[ApiController]
+[Route("api/auth")]
+public class AuthenticationController : ControllerBase
+{
+    private readonly IAuthenticationService _authorizationService;
+
+    public AuthenticationController(IAuthenticationService authorizationService)
+    {
+        _authorizationService = authorizationService;
+    }
+
+    [HttpPost("register")]
+    public IActionResult Register(RegisterRequest request)
+    {
+        var authResult = _authorizationService.Register(request.FirstName, request.LastName, request.Email, request.Password);
+
+        var response = new AuthenticationResponse(
+            authResult.Id,
+            authResult.FirstName,
+            authResult.LastName,
+            authResult.Email,
+            authResult.Token);
+
+        return Ok(response);
+    }
+
+    [HttpPost("login")]
+    public IActionResult Login(LoginRequest request)
+    {
+        var authResult = _authorizationService.Login(request.Email, request.Password);
+
+        var response = new AuthenticationResponse(
+            authResult.Id,
+            authResult.FirstName,
+            authResult.LastName,
+            authResult.Email,
+            authResult.Token);
+
+        return Ok(response);
+    }
+}

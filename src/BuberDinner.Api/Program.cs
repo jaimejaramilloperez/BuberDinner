375bf4554e0c3a1104b40d8c2faebef4e0f22735
Jaime Jaramillo <jaimejaramillo.dev@gmail.com>
2024-05-29 10:26:20 -0500

    Configure Api dependency injection with application and infrastructure dependencies

--- src/BuberDinner.Api/Program.cs
+++ src/BuberDinner.Api/Program.cs
@@ -1,4 +1,10 @@
+using BuberDinner.Application;
+using BuberDinner.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure(builder.Configuration);
 
 builder.Services.AddControllers();
 

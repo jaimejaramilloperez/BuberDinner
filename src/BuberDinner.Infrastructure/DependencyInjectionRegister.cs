0a7ae0968d34a764d86ca9fbfd0521dac749232e
Jaime Jaramillo <jaimejaramillo.dev@gmail.com>
2024-05-29 10:21:28 -0500

    Add infrastructure dependency injection registration

---
+++ src/BuberDinner.Infrastructure/DependencyInjectionRegister.cs
@@ -0,0 +1,21 @@
+﻿using BuberDinner.Application.Common.Interfaces.Authentication;
+using BuberDinner.Application.Common.Interfaces.Services;
+using BuberDinner.Infrastructure.Authentication;
+using BuberDinner.Infrastructure.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BuberDinner.Infrastructure;
+
+public static class DependencyInjectionRegister
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, ConfigurationManager configuration)
+    {
+        services.Configure<JwtSettings>(configuration.GetSection(JwtSettings.ConfigurationSection));
+
+        services.AddSingleton<IDateTimeProvider, DateTimeProvider>();
+        services.AddSingleton<IJwtTokenGenerator, JwtTokenGenerator>();
+
+        return services;
+    }
+}

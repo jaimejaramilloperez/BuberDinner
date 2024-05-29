ee9e11af9e37f9e00c6fc379d7de4f4bccea9260
Jaime Jaramillo <jaimejaramillo.dev@gmail.com>
2024-05-29 10:25:02 -0500

    Add application dependency injection registration

---
+++ src/BuberDinner.Application/DependencyInjectionRegister.cs
@@ -0,0 +1,14 @@
+﻿using BuberDinner.Application.Autentication;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BuberDinner.Application;
+
+public static class DependencyInjectionRegister
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddScoped<IAuthenticationService, AuthenticationService>();
+
+        return services;
+    }
+}

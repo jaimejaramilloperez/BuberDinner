38e1b5e81536fa36160aae584408b7ae6b0ff33f
Jaime Jaramillo <jaimejaramillo.dev@gmail.com>
2024-05-30 10:57:47 -0500

    Add user repository interface and implementation

---
+++ src/BuberDinner.Infrastructure/Persistence/UserRepository.cs
@@ -0,0 +1,18 @@
+﻿using BuberDinner.Application.Common.Interfaces.Persistence;
+using BuberDinner.Domain.Entities;
+
+namespace BuberDinner.Infrastructure.Persistence;
+
+internal class UserRepository : IUserRepository
+{
+    private static readonly List<User> Users = [];
+
+    public User? GetUserByEmail(string email)
+    {
+        return Users.Find(x => x.Email == email);
+    }
+    public void Add(User user)
+    {
+        Users.Add(user);
+    }
+}

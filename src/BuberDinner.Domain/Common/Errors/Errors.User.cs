82fa2edfe2e995e2dd61f73897c237b1233db0c3
Jaime Jaramillo <jaimejaramillo.dev@gmail.com>
2024-05-30 17:19:01 -0500

    Add User and Authentication domain errors

---
+++ src/BuberDinner.Domain/Common/Errors/Errors.User.cs
@@ -0,0 +1,13 @@
+﻿using ErrorOr;
+
+namespace BuberDinner.Domain.Common.Errors;
+
+public static partial class Errors
+{
+    public static class User
+    {
+        public static Error DuplicatedEmail => Error.Conflict(
+            code: "User.DuplicatedEmail",
+            description: "Email is already in use.");
+    }
+}

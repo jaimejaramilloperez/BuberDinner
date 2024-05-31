e26a24f4f92a24e5cb2f8cfeda417eaa2583f1d6
Jaime Jaramillo <jaimejaramillo.dev@gmail.com>
2024-05-31 12:29:23 -0500

    Configure authentication mapping

---
+++ src/BuberDinner.Api/Mapping/AuthenticationMappingConfig.cs
@@ -0,0 +1,20 @@
+﻿using BuberDinner.Application.Autentication.Common;
+using BuberDinner.Application.Autentication.Login;
+using BuberDinner.Application.Autentication.Register;
+using BuberDinner.Contracts;
+using Mapster;
+
+namespace BuberDinner.Api.Mapping;
+
+public class AuthenticationMappingConfig : IRegister
+{
+    public void Register(TypeAdapterConfig config)
+    {
+        config.NewConfig<RegisterRequest, RegisterCommand>();
+
+        config.NewConfig<LoginQuery, LoginQuery>();
+
+        config.NewConfig<AuthenticationResult, AuthenticationResponse>()
+            .Map(dest => dest, src => src.User);
+    }
+}

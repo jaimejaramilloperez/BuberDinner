3609eca94e6b1f1ec85ce6f95d3a02e663a0f804
Jaime Jaramillo <jaimejaramillo.dev@gmail.com>
2024-05-31 12:32:19 -0500

    Update presentation layer dependency injection

---
+++ src/BuberDinner.Api/DependencyInjectionRegister.cs
@@ -0,0 +1,17 @@
+﻿using BuberDinner.Api.Common.Errors;
+using BuberDinner.Api.Mapping;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+
+namespace BuberDinner.Api;
+
+public static class DependencyInjectionRegister
+{
+    public static IServiceCollection AddPresentation(this IServiceCollection services)
+    {
+        services.AddControllers();
+        services.AddMappings();
+        services.AddSingleton<ProblemDetailsFactory, BuberDinnerProblemDetailsFactory>();
+
+        return services;
+    }
+}
--- src/BuberDinner.Api/Program.cs
+++ src/BuberDinner.Api/Program.cs
@@ -1,16 +1,12 @@
-using BuberDinner.Api.Common.Errors;
+using BuberDinner.Api;
 using BuberDinner.Application;
 using BuberDinner.Infrastructure;
-using Microsoft.AspNetCore.Mvc.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
-
-builder.Services.AddControllers();
-
-builder.Services.AddSingleton<ProblemDetailsFactory, BuberDinnerProblemDetailsFactory>();
+builder.Services.AddPresentation();
 
 var app = builder.Build();
 

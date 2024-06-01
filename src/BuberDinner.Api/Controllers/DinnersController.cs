52f15e32c5f2e7d0b42482348b81573f059736e0
Jaime Jaramillo <jaimejaramillo.dev@gmail.com>
2024-06-01 09:59:01 -0500

    Add DinnersController and request file

---
+++ src/BuberDinner.Api/Controllers/DinnersController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BuberDinner.Api.Controllers;
+
+[Route("api/dinners")]
+public class DinnersController : ApiController
+{
+    [HttpGet]
+    public IActionResult ListDinners()
+    {
+        return Ok(Array.Empty<string>());
+    }
+}

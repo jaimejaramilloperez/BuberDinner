32d3d111483bdcadd7fd4f50e27d723eb21a1663
Jaime Jaramillo <jaimejaramillo.dev@gmail.com>
2024-06-02 11:14:01 -0500

    Add AggregateRoot for MenuReview object

---
+++ src/BuberDinner.Domain/MenuReview/MenuReview.cs
@@ -0,0 +1,59 @@
+﻿using BuberDinner.Domain.Common.Models;
+using BuberDinner.Domain.Common.ValueObjects;
+using BuberDinner.Domain.Dinner.ValueObjects;
+using BuberDinner.Domain.Guest.ValueObjects;
+using BuberDinner.Domain.Host.ValueObjects;
+using BuberDinner.Domain.Menu.ValueObjects;
+using BuberDinner.Domain.MenuReview.ValueObjects;
+
+namespace BuberDinner.Domain.MenuReview;
+
+public sealed class MenuReview : AggregateRoot<MenuReviewId>
+{
+    public Rating Rating { get; private set; }
+    public string Comment { get; private set; }
+    public HostId HostId { get; private set; }
+    public MenuId MenuId { get; private set; }
+    public GuestId GuestId { get; private set; }
+    public DinnerId DinnerId { get; private set; }
+    public DateTime CreatedDateTime { get; private set; }
+    public DateTime UpdatedDateTime { get; private set; }
+
+    private MenuReview(
+        MenuReviewId menuReviewId,
+        HostId hostId,
+        MenuId menuId,
+        GuestId guestId,
+        DinnerId dinnerId,
+        Rating rating,
+        string comment)
+        : base(menuReviewId)
+    {
+        Rating = rating;
+        Comment = comment;
+        HostId = hostId;
+        MenuId = menuId;
+        GuestId = guestId;
+        DinnerId = dinnerId;
+        CreatedDateTime = DateTime.UtcNow;
+        UpdatedDateTime = DateTime.UtcNow;
+    }
+
+    public static MenuReview Create(
+        HostId hostId,
+        MenuId menuId,
+        GuestId guestId,
+        DinnerId dinnerId,
+        double rating,
+        string comment)
+    {
+        return new MenuReview(
+            MenuReviewId.CreateUnique(),
+            hostId,
+            menuId,
+            guestId,
+            dinnerId,
+            Rating.CreateNew(rating),
+            comment);
+    }
+}

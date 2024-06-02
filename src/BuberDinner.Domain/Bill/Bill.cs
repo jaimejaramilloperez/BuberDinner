4d31695d8c029fe6a8cb188cb831eceff96d95c9
Jaime Jaramillo <jaimejaramillo.dev@gmail.com>
2024-06-02 09:46:51 -0500

    Add AggregateRoot, Entites and VaueObjects for Bill object

---
+++ src/BuberDinner.Domain/Bill/Bill.cs
@@ -0,0 +1,48 @@
+﻿using BuberDinner.Domain.Bill.ValueObjects;
+using BuberDinner.Domain.Common.Models;
+using BuberDinner.Domain.Dinner.ValueObjects;
+using BuberDinner.Domain.Guest.ValueObjects;
+using BuberDinner.Domain.Host.ValueObjects;
+
+namespace BuberDinner.Domain.Bill;
+
+public sealed class Bill : AggregateRoot<BillId>
+{
+    public DinnerId DinnerId { get; private set; }
+    public GuestId GuestId { get; private set; }
+    public HostId HostId { get; private set; }
+    public Price Price { get; private set; }
+    public DateTime CreatedDateTime { get; private set; }
+    public DateTime UpdatedDateTime { get; private set; }
+
+    private Bill(
+        BillId billId,
+        DinnerId dinnerId,
+        GuestId guestId,
+        HostId hostId,
+        Price price)
+        : base(billId)
+    {
+        DinnerId = dinnerId;
+        GuestId = guestId;
+        HostId = hostId;
+        Price = price;
+        CreatedDateTime = DateTime.UtcNow;
+        UpdatedDateTime = DateTime.UtcNow;
+    }
+
+    public static Bill Create(
+        DinnerId dinnerId,
+        GuestId guestId,
+        HostId hostId,
+        decimal amount,
+        string currency)
+    {
+        return new Bill(
+            BillId.CreateUnique(),
+            dinnerId,
+            guestId,
+            hostId,
+            Price.CreateNew(amount, currency));
+    }
+}

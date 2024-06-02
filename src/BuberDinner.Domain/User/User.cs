59b13803bbfc5a6002e883c7a1d12353a54eb956
Jaime Jaramillo <jaimejaramillo.dev@gmail.com>
2024-06-02 09:54:51 -0500

    Add AggregateRoot and VaueObject for User object

---
+++ src/BuberDinner.Domain/User/User.cs
@@ -0,0 +1,44 @@
+﻿using BuberDinner.Domain.Common.Models;
+using BuberDinner.Domain.User.ValueObjects;
+
+namespace BuberDinner.Domain.Entities;
+
+public sealed class User : AggregateRoot<UserId>
+{
+    public string FirstName { get; private set; }
+    public string LastName { get; private set; }
+    public string Email { get; private set; }
+    public string Password { get; private set; }
+    public DateTime CreatedDateTime { get; private set; }
+    public DateTime UpdatedDateTime { get; private set; }
+
+    private User(
+        UserId userId,
+        string firstName,
+        string lastName,
+        string email,
+        string password)
+        : base(userId)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Email = email;
+        Password = password;
+        CreatedDateTime = DateTime.UtcNow;
+        UpdatedDateTime = DateTime.UtcNow;
+    }
+
+    public static User Create(
+        string firstName,
+        string lastName,
+        string email,
+        string password)
+    {
+        return new User(
+            UserId.CreateUnique(),
+            firstName,
+            lastName,
+            email,
+            password);
+    }
+}

e30c8bc2eed79bf9eeb32cd9693f3a8b380ad564
Jaime Jaramillo <jaimejaramillo.dev@gmail.com>
2024-06-02 09:45:11 -0500

    Refactor MenuSection class to initialize _items list in constructor

--- src/BuberDinner.Domain/Menu/Entities/MenuSection.cs
+++ src/BuberDinner.Domain/Menu/Entities/MenuSection.cs
@@ -5,7 +5,7 @@ namespace BuberDinner.Domain.Menu.Entities;
 
 public sealed class MenuSection : Entity<MenuSectionId>
 {
-    private readonly List<MenuItem> _items = [];
+    private readonly List<MenuItem> _items;
 
     public string Name { get; }
     public string Description { get; }
@@ -14,20 +14,24 @@ public sealed class MenuSection : Entity<MenuSectionId>
     public MenuSection(
         MenuSectionId menuSectionId,
         string name,
-        string description)
+        string description,
+        List<MenuItem> items)
         : base(menuSectionId)
     {
         Name = name;
         Description = description;
+        _items = items;
     }
 
     public static MenuSection Create(
         string name,
-        string description)
+        string description,
+        List<MenuItem>? items = null)
     {
         return new MenuSection(
             MenuSectionId.CreateUnique(),
             name,
-            description);
+            description,
+            items ?? []);
     }
 }

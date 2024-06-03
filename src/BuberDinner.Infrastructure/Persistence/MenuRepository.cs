b515337927c9637ad8ff361f02ee680760318ea3
Jaime Jaramillo <jaimejaramillo.dev@gmail.com>
2024-06-03 18:46:36 -0500

    Add menu repository interface and implementation

---
+++ src/BuberDinner.Infrastructure/Persistence/MenuRepository.cs
@@ -0,0 +1,14 @@
+﻿using BuberDinner.Application.Common.Interfaces.Persistence;
+using BuberDinner.Domain.Menu;
+
+namespace BuberDinner.Infrastructure.Persistence;
+
+public class MenuRepository : IMenuRepository
+{
+    private static readonly List<Menu> Menus = [];
+
+    public void Add(Menu menu)
+    {
+        Menus.Add(menu);
+    }
+}

a5dd5f52f60f324f298861f36332f6de3d605c69
Jaime Jaramillo <jaimejaramillo.dev@gmail.com>
2024-06-03 18:50:34 -0500

    Add CreateMenu command, handler and validator

---
+++ src/BuberDinner.Application/Menus/CreateMenu/CreateMenuCommandHandler.cs
@@ -0,0 +1,36 @@
+﻿using BuberDinner.Application.Common.Interfaces.Persistence;
+using BuberDinner.Domain.Host.ValueObjects;
+using BuberDinner.Domain.Menu;
+using BuberDinner.Domain.Menu.Entities;
+using ErrorOr;
+using MediatR;
+
+namespace BuberDinner.Application.Menus.CreateMenu;
+
+public class CreateMenuCommandHandler : IRequestHandler<CreateMenuCommand, ErrorOr<Menu>>
+{
+    private readonly IMenuRepository _menuRepository;
+
+    public CreateMenuCommandHandler(IMenuRepository menuRepository)
+    {
+        _menuRepository = menuRepository;
+    }
+
+    public async Task<ErrorOr<Menu>> Handle(CreateMenuCommand request, CancellationToken cancellationToken)
+    {
+        var menu = Menu.Create(
+            hostId: HostId.Create(Guid.Parse(request.HostId)),
+            name: request.Name,
+            description: request.Description,
+            sections: request.Sections.ConvertAll(section => MenuSection.Create(
+                name: section.Name,
+                description: section.Description,
+                items: section.Items.ConvertAll(item => MenuItem.Create(
+                    name: item.Name,
+                    description: item.Description)))));
+
+        _menuRepository.Add(menu);
+
+        return await Task.FromResult(menu);
+    }
+}

a9cc74881affa8e85f6692b4456ef6cb2622d326
Jaime Jaramillo <jaimejaramillo.dev@gmail.com>
2024-06-03 18:52:27 -0500

    Add MenusController with CreateMenu endpoint

---
+++ src/BuberDinner.Api/Controllers/MenusController.cs
@@ -0,0 +1,32 @@
+﻿using BuberDinner.Application.Menus.CreateMenu;
+using BuberDinner.Contracts.Menus;
+using MapsterMapper;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuberDinner.Api.Controllers;
+
+[Route("api/hosts/{hostId}/menus")]
+public class MenusController : ApiController
+{
+    private readonly ISender _mediator;
+    private readonly IMapper _mapper;
+
+    public MenusController(IMapper mapper, ISender mediator)
+    {
+        _mapper = mapper;
+        _mediator = mediator;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateMenu(string hostId, CreateMenuRequest request, CancellationToken cancellationToken)
+    {
+        var command = _mapper.Map<CreateMenuCommand>((request, hostId));
+
+        var createMenuResult = await _mediator.Send(command, cancellationToken);
+
+        return createMenuResult.Match(
+            menu => Ok(_mapper.Map<MenuResponse>(menu)),
+            errors => Problem(errors));
+    }
+}

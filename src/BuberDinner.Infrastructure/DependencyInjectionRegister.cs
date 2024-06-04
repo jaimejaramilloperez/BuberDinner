1b12e418ee329a86bcfd29e61c3824df729d999e
Jaime Jaramillo <jaimejaramillo.dev@gmail.com>
2024-06-04 16:07:04 -0500

    Add entityframework in infrastructure depencency injection

--- src/BuberDinner.Infrastructure/DependencyInjectionRegister.cs
+++ src/BuberDinner.Infrastructure/DependencyInjectionRegister.cs
@@ -4,8 +4,10 @@ using BuberDinner.Application.Common.Interfaces.Persistence;
 using BuberDinner.Application.Common.Interfaces.Services;
 using BuberDinner.Infrastructure.Authentication;
 using BuberDinner.Infrastructure.Persistence;
+using BuberDinner.Infrastructure.Persistence.Repositories;
 using BuberDinner.Infrastructure.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
@@ -19,15 +21,28 @@ public static class DependencyInjectionRegister
     {
         services.AddSingleton<IDateTimeProvider, DateTimeProvider>();
 
-        services.AddPersistence();
+        services.AddPersistence(configuration);
 
         services.AddAuth(configuration);
 
         return services;
     }
 
-    private static IServiceCollection AddPersistence(this IServiceCollection services)
+    private static IServiceCollection AddPersistence(this IServiceCollection services, ConfigurationManager configuration)
     {
+        services.AddDbContext<BuberDinnerDbContext>(options =>
+        {
+            options.UseSqlServer(configuration.GetConnectionString("Default"), sqlServerOptions =>
+            {
+                sqlServerOptions.EnableRetryOnFailure(
+                    maxRetryCount: 3,
+                    maxRetryDelay: TimeSpan.FromSeconds(2),
+                    errorNumbersToAdd: []);
+
+                sqlServerOptions.CommandTimeout(20);
+            });
+        });
+
         services.AddScoped<IUserRepository, UserRepository>();
         services.AddScoped<IMenuRepository, MenuRepository>();
 

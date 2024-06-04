e0c1d50033ec28a07f7f70ceaa18b78153b115e4
Jaime Jaramillo <jaimejaramillo.dev@gmail.com>
2024-06-04 16:02:24 -0500

    Add BuberDinnerDbContext with menu configuration and dbset

---
+++ src/BuberDinner.Infrastructure/Persistence/BuberDinnerDbContext.cs
@@ -0,0 +1,22 @@
+﻿using BuberDinner.Domain.Menu;
+using BuberDinner.Infrastructure.Persistence.Configurations;
+using Microsoft.EntityFrameworkCore;
+
+namespace BuberDinner.Infrastructure.Persistence;
+
+public class BuberDinnerDbContext : DbContext
+{
+    public BuberDinnerDbContext(DbContextOptions<BuberDinnerDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfiguration(new MenuConfigurations());
+    }
+
+    public DbSet<Menu> Menus { get; set; }
+}

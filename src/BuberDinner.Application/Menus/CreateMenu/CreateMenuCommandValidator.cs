65affc1f9342a886b7a017ecf7ebf00a23bcc37b
Jaime Jaramillo <jaimejaramillo.dev@gmail.com>
2024-06-04 16:17:57 -0500

    Update CreateMenuCommandValidator to validate HostId

--- src/BuberDinner.Application/Menus/CreateMenu/CreateMenuCommandValidator.cs
+++ src/BuberDinner.Application/Menus/CreateMenu/CreateMenuCommandValidator.cs
@@ -6,6 +6,7 @@ public class CreateMenuCommandValidator : AbstractValidator<CreateMenuCommand>
 {
     public CreateMenuCommandValidator()
     {
+        RuleFor(x => x.HostId).NotEmpty();
         RuleFor(x => x.Name).NotEmpty();
         RuleFor(x => x.Description).NotEmpty();
         RuleFor(x => x.Sections).NotEmpty();

4c8c5b39921c0767b227229adb138de9fd7ccfa9
Jaime Jaramillo <jaimejaramillo.dev@gmail.com>
2024-06-05 13:52:04 -0500

    Add PublishDomainEventInterceptor to notify events to the Mediator

---
+++ src/BuberDinner.Infrastructure/Persistence/Interceptors/PublishDomainEventInterceptor.cs
@@ -0,0 +1,60 @@
+﻿using BuberDinner.Domain.Common.Models;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+namespace BuberDinner.Infrastructure.Persistence.Interceptors;
+
+public class PublishDomainEventInterceptor : SaveChangesInterceptor
+{
+    private readonly IPublisher _mediator;
+
+    public PublishDomainEventInterceptor(IPublisher mediator)
+    {
+        _mediator = mediator;
+    }
+
+    public override InterceptionResult<int> SavingChanges(
+        DbContextEventData eventData,
+        InterceptionResult<int> result)
+    {
+        PublishDomainEvents(eventData.Context).GetAwaiter().GetResult();
+        return base.SavingChanges(eventData, result);
+    }
+
+    public override async ValueTask<InterceptionResult<int>> SavingChangesAsync(
+        DbContextEventData eventData,
+        InterceptionResult<int> result,
+        CancellationToken cancellationToken = default)
+    {
+        await PublishDomainEvents(eventData.Context, cancellationToken);
+        return await base.SavingChangesAsync(eventData, result, cancellationToken);
+    }
+
+    private async Task PublishDomainEvents(DbContext? dbcContext, CancellationToken cancellationToken = default)
+    {
+        if (dbcContext is null)
+        {
+            return;
+        }
+
+        var entititesWithDomainEvents = dbcContext.ChangeTracker.Entries<IHasDomainEvents>()
+            .Where(entry => entry.Entity.DomainEvents.Any())
+            .Select(entry => entry.Entity)
+            .ToList();
+
+        var domainEvents = entititesWithDomainEvents
+            .SelectMany(entry => entry.DomainEvents)
+            .ToList();
+
+        foreach (var entity in entititesWithDomainEvents)
+        {
+            entity.ClearDomainEvents();
+        }
+
+        foreach (var domainEvent in domainEvents)
+        {
+            await _mediator.Publish(domainEvent, cancellationToken);
+        }
+    }
+}

0e528b68db97adcdfa7e7c79a765729c1c252b94
Jaime Jaramillo <jaimejaramillo.dev@gmail.com>
2024-06-05 13:55:43 -0500

    Add dummy handler for MenuCreated event

---
+++ src/BuberDinner.Application/Menus/CreateMenu/MenuCreatedHandler.cs
@@ -0,0 +1,12 @@
+﻿using BuberDinner.Domain.Menu.Events;
+using MediatR;
+
+namespace BuberDinner.Application.Menus.CreateMenu;
+
+public class MenuCreatedHandler : INotificationHandler<MenuCreated>
+{
+    public async Task Handle(MenuCreated notification, CancellationToken cancellationToken)
+    {
+        await Task.CompletedTask;
+    }
+}

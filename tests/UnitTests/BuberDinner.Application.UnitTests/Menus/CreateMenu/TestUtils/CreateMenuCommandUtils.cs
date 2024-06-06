f7120c41840121a079406ea15d8b565ff2ff00d5
Jaime Jaramillo <jaimejaramillo.dev@gmail.com>
2024-06-06 13:03:46 -0500

    test: Add CreateMenuCommandUtils class for creating the command object

---
+++ tests/UnitTests/BuberDinner.Application.UnitTests/Menus/CreateMenu/TestUtils/CreateMenuCommandUtils.cs
@@ -0,0 +1,37 @@
+﻿using BuberDinner.Application.Menus.CreateMenu;
+using BuberDinner.Application.UnitTests.TestUtils.Constants;
+
+namespace BuberDinner.Application.UnitTests.Menus.CreateMenu.TestUtils;
+
+public static class CreateMenuCommandUtils
+{
+    public static CreateMenuCommand CreateCommand(List<CreateMenuSectionCommand>? sections = null)
+    {
+        return new CreateMenuCommand(
+            HostId: Constants.Host.Id,
+            Name: Constants.Menu.Name,
+            Description: Constants.Menu.Description,
+            Sections: sections ?? CreateMenuSections());
+    }
+
+    public static List<CreateMenuSectionCommand> CreateMenuSections(
+        int sectionCount = 1,
+        List<CreateMenuItemCommand>? items = null)
+    {
+        return Enumerable.Range(0, sectionCount)
+            .Select(index => new CreateMenuSectionCommand(
+                Name: Constants.Menu.SectionNameFromIndex(index),
+                Description: Constants.Menu.SectionDescriptionFromIndex(index),
+                Items: items ?? CreateMenuItems()))
+            .ToList();
+    }
+
+    public static List<CreateMenuItemCommand> CreateMenuItems(int itemCount = 1)
+    {
+        return Enumerable.Range(0, itemCount)
+            .Select(index => new CreateMenuItemCommand(
+                Name: Constants.Menu.ItemNameFromIndex(index),
+                Description: Constants.Menu.ItemDescriptionFromIndex(index)))
+            .ToList();
+    }
+}

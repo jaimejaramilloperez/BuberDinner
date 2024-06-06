7a358b0b55735c95c274f22aad2351528db0dc90
Jaime Jaramillo <jaimejaramillo.dev@gmail.com>
2024-06-06 13:05:36 -0500

    test: Add the Menu extension method to validate the command object

---
+++ tests/UnitTests/BuberDinner.Application.UnitTests/TestUtils/Menus/Extensions/MenuExtensions.Validation.cs
@@ -0,0 +1,33 @@
+﻿using BuberDinner.Application.Menus.CreateMenu;
+using BuberDinner.Domain.Menu;
+using BuberDinner.Domain.Menu.Entities;
+using FluentAssertions;
+
+namespace BuberDinner.Application.UnitTests.TestUtils.Menus.Extensions;
+
+public static partial class MenuExtensions
+{
+    public static void ValidateCreatedFrom(this Menu menu, CreateMenuCommand command)
+    {
+        menu.Name.Should().Be(command.Name);
+        menu.Description.Should().Be(command.Description);
+        menu.Sections.Should().HaveSameCount(command.Sections);
+        menu.Sections.Zip(command.Sections).ToList().ForEach(pair => ValidateSection(pair.First, pair.Second));
+
+        static void ValidateSection(MenuSection section, CreateMenuSectionCommand menuSection)
+        {
+            section.Id.Should().NotBeNull();
+            section.Name.Should().Be(menuSection.Name);
+            section.Description.Should().Be(menuSection.Description);
+            section.Items.Should().HaveSameCount(menuSection.Items);
+            section.Items.Zip(menuSection.Items).ToList().ForEach(pair => ValidateItem(pair.First, pair.Second));
+        }
+
+        static void ValidateItem(MenuItem item, CreateMenuItemCommand menuItem)
+        {
+            item.Id.Should().NotBeNull();
+            item.Name.Should().Be(menuItem.Name);
+            item.Description.Should().Be(menuItem.Description);
+        }
+    }
+}

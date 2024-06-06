f437d9443ffd48ab281764eebefcf439fe11e2fd
Jaime Jaramillo <jaimejaramillo.dev@gmail.com>
2024-06-06 13:02:24 -0500

    test: Add menu and host constants in test utils folder

---
+++ tests/UnitTests/BuberDinner.Application.UnitTests/TestUtils/Constants/Constants.Host.cs
@@ -0,0 +1,9 @@
+﻿namespace BuberDinner.Application.UnitTests.TestUtils.Constants;
+
+public static partial class Constants
+{
+    public static class Host
+    {
+        public static readonly string Id = "757b6bc3-50d6-423f-818e-f849ae3fc7bc";
+    }
+}
